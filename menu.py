da84695574fe2b7674c205b0cedab94da661f97b
mohamedgarde46-maker <mohamedgarde46@gmail.com>
2026-07-17 17:10:11 -0400

    Add main_menu function to menu.py

--- menu.py
+++ menu.py
@@ -10,3 +10,18 @@ def show_menu():
     console.print("[green]4.[/green] Hash Generator")
     console.print("[green]5.[/green] About")
     console.print("[red]0.[/red] Exit")
+    
+    def main_menu():
+    print("1. Network Scanner")
+    print("2. Port Scanner")
+    
+    choice = input(" -> ")
+    
+    if choice == '1':
+        network_scanner() # هنا سيقوم البرنامج بتشغيل كودك القديم!
+    elif choice == '2':
+        port_scanner()    # هنا سيشغل الأداة الجديدة
+
+# هذا السطر يخبر بايثون أن يبدأ تشغيل البرنامج من القائمة الرئيسية
+if __name__ == "__main__":
+    main_menu()
