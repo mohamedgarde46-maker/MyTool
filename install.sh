e2941c2e4afd0177bf039f419f79b3ed4fb69966
mohamedgarde46-maker <mohamedgarde46@gmail.com>
2026-07-17 16:49:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,18 @@ chmod +x /usr/local/bin/mytool
 echo "============================================="
 echo "  [+] Done! You can now run the tool using: mytool"
 echo "============================================="
+#!/bin/bash
+
+# تثبيت المكتبات الخاصة بتعديل الخطوط واللغات الشرقية
+pip3 install arabic-reshaper python-bidi --break-system-packages
+
+# إنشاء مجلد ثابت للأداة داخل النظام
+mkdir -p /usr/share/mytool
+cp -r * /usr/share/mytool/
+
+# إنشاء أمر اختصار في النظام باسم mytool
+echo '#!/bin/bash' > /usr/local/bin/mytool
+echo 'cd /usr/share/mytool && python3 ku_netscan.py "$@"' >> /usr/local/bin/mytool
+chmod +x /usr/local/bin/mytool
+
+echo "Done!"
